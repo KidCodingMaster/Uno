c964d7bf3afe95f7c33c9f3e0b0fea4a25be4b94
KidCodingMaster <lenovotab12101@gmail.com>
2025-09-27 18:57:11 +0400

    Stated main user interaction and made player and opponent deck

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import random
 
+
 class Card:
     def __init__(self, color: str, number: str) -> None:
         self.color = color
@@ -27,3 +28,23 @@ def display_all_cards(cards: list[Card], print_: bool = True) -> str:
         print(printed_str)
 
     return printed_str
+
+
+def get_random_card():
+    return Card(random.choice(CARD_COLORS), random.choice(CARD_NUMBERS))
+
+
+CARD_NUMBERS = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+CARD_COLORS = ["Red", "Blue", "Green", "Yellow"]
+
+player_deck = [get_random_card() for _ in range(7)]
+opponent_deck = [get_random_card() for _ in range(7)]
+discard_pile = [Card(random.choice(CARD_COLORS), random.choice(CARD_NUMBERS))]
+
+while True:
+    cmd = input("What do you want to do? (view - view your cards)")
+
+    match cmd:
+        case "view":
+            print("Your Cards:")
+            display_all_cards(player_deck)
